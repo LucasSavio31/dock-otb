d2d3a8446ee7d282277728f5f186520da0713bca
Lucas Savio de Oliveira <lucas.savio@sp.senai.br>
2026-04-14 09:22:06 -0300

    chore: remove .pio e .vscode do repositorio

--- .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
