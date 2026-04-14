ae149a1a3dda94413ef8bc4ac49833196efa9fd9
Lucas Savio de Oliveira <lucas.savio@sp.senai.br>
2026-04-14 09:14:15 -0300

    feat: V5 - forcando subida completa dos arquivos

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/sn1098525/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
